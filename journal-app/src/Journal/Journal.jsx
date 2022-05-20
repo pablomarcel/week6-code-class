0550ac0b516c71d96937dcc5872c3683e1361992
pablomarcel <pablomarcel@outlook.com>
2022-05-19 19:07:15 -0700

    useSnapshot

--- journal-app/src/Journal/Journal.jsx
+++ journal-app/src/Journal/Journal.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
 
 import db from '../db'
 
@@ -10,26 +10,40 @@ export default function Journal() {
     const [error, setError]=useState(false)
 
     useEffect(()=>{
-        getDocs(
-            collection(db,'journalEntries')
-        ).then(
-            snapshot=>{
+        // getDocs(
+        //     collection(db,'journalEntries')
+        // ).then(
+        //     snapshot=>{
+        //
+        //         // console.log(snapshot.map(doc=>doc.id))
+        //         setEntries(snapshot.docs)
+        //         setLoading(false)
+        //         // snapshot.forEach(doc=>{
+        //         //     console.log(
+        //         //         doc.id,doc.data()
+        //         //     )
+        //         // })
+        //     },
+        //     reason => {
+        //         setError(true)
+        //         setLoading(false)
+        //
+        //     }
+        // )
 
-                // console.log(snapshot.map(doc=>doc.id))
+        onSnapshot(
+            collection(db,'journalEntries'),
+            snapshot => {
                 setEntries(snapshot.docs)
                 setLoading(false)
-                // snapshot.forEach(doc=>{
-                //     console.log(
-                //         doc.id,doc.data()
-                //     )
-                // })
             },
-            reason => {
+            reason=>{
                 setError(true)
                 setLoading(false)
-
             }
         )
+
+
     }, [])
 
     if (error){
